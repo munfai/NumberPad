fe195272514d679df6a9aef3422fbde518057b68
MunFai <munfai@live.com>
2015-08-06 22:34:47 +0800

    Minor edit

--- NumberPad/NumberPad/NumberPadView/GBNumberPad.h
+++ NumberPad/NumberPad/NumberPadView/GBNumberPad.h
@@ -1,9 +1,9 @@
 //
 //  GBNumberPad.h
-//  test
+//  NumberPad
 //
-//  Created by Administrator on 4/14/15.
-//  Copyright (c) 2015 Globeam. All rights reserved.
+//  Created by Mun Fai Leong on 8/5/15.
+//  Copyright (c) 2015 munfai. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
